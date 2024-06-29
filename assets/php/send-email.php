d39d9f7e37b38d7800df48785375d5491d7b2c89
hkumar1996 <hemanthkumarchelakacherla@gmail.com>
2024-06-29 15:13:12 +0530

    Initial commit: Added my website files

---
+++ assets/php/send-email.php
@@ -0,0 +1,27 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = htmlspecialchars($_POST['name']);
+    $email = filter_var($_POST['email'], FILTER_SANITIZE_EMAIL);
+    $message = htmlspecialchars($_POST['message']);
+
+    // Your email address where you want to receive the emails
+    $to = "hemanthkumarchelakacherla@gmail.com";  
+    $subject = "New Hire Request";
+    $body = "Name: $name\nEmail: $email\nMessage: $message";
+    $headers = "From: $email";
+
+    // Check if the email address is valid
+    if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        // Send the email
+        if (mail($to, $subject, $body, $headers)) {
+            echo "Email sent successfully!";
+        } else {
+            echo "Failed to send email.";
+        }
+    } else {
+        echo "Invalid email address.";
+    }
+} else {
+    echo "Invalid request method.";
+}
+?>
